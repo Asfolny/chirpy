d87ce7de396faa1edf7b5c59e4aa720090e8c58a
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-15 23:46:14 +0200

    Add middleware to track user visits

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		cfg.fileserverHits++
+		writer.Header().Set("Cache-Control", "no-cache")
+		next.ServeHTTP(writer, request)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{}
+
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))),
+	)
 	mux.HandleFunc("/healthz", health)
+	mux.HandleFunc("/metrics", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+		fmt.Fprintf(writer, "Hits: %v", apiCfg.fileserverHits)
+	})
+	mux.HandleFunc("/reset", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		writer.WriteHeader(http.StatusOK)
+		apiCfg.fileserverHits = 0
+	})
 
 	s := &http.Server{
 		Addr:    ":8080",
