416c52f50f0f61f757f701371f7239386b124179
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-15 23:46:02 +0200

    Added health endpoint

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", health)
 
 	s := &http.Server{
 		Addr:    ":8080",
@@ -16,4 +17,10 @@ func main() {
 	}
 
 	log.Fatal(s.ListenAndServe())
+}
+
+func health(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(writer, "OK")
 }
