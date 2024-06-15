07adf2ee7d38174e541b0fcfd231aa99672abfe2
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-15 23:43:49 +0200

    Moved index to /app/ prefix

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	s := &http.Server{
 		Addr:    ":8080",
